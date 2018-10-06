e3c0a7e182ac52375c8933f633d00b36165eabd3
hoptond <hoptond848@protonmail.com>
2018-10-06 11:02:28 +0100

    made sprite class fields more sensible

--- Components/Sprite.cs
+++ Components/Sprite.cs
@@ -5,171 +5,161 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Media;
 
 namespace Magicians
 {
-    class Sprite : IDisposable
+    class Sprite
     {
-        public Texture2D spriteSheet { get; private set; }
-        public int currentFrame { get; set; }
-        Point drawnPosition; //the position at which the object is drawn.
-        public Point DrawnPosition { get { return drawnPosition; } }
-        public Point spriteSize;
-        public int alpha = 255;
+        public Texture2D SpriteSheet { get; private set; }
+        public int CurrentFrame { get; set; }
+		public Point DrawnPosition { get; private set; } //the position at which the object is drawn.
+		public Point SpriteSize { get; private set; }
+		public Rectangle DrawnBounds { get; private set; }
+        public Rectangle SpriteRect { get; private set; }
+		public float Depth { get; private set; }
+        public float Interval { get; private set; }
+        public int BottomY { get; private set; } //the "bottom" of the sprite      
+		public bool ReachedEnd { get; private set; }
+		public bool IgnoreDepthSorting { get; private set; }
+		public string GraphicsDir { get; private set; }
+		public enum OriginType { TopLeft, FromCentre, BottomLeft, BottomMiddle };
+		public OriginType OriginMode { get; private set; }
+        public bool Scaled { get; private set; } //if this is set to true, then the sprite is Scaled up to the specified width and height
+        public int scaleWidth { get; private set; }
+        public int scaleHeight { get; private set; }
 
-        Rectangle drawBounds;
-        public Rectangle DrawnBounds { get { return drawBounds; } }
-        public Rectangle spriteRect { get; private set; }
-
+		int alpha = 255;
         float rotation;
-        public float depth { get; private set; }
-		float timer;
-        public float interval { get; private set; }
+        float timer;
         int bottomYOffset;
-        public int bottomY; //the "bottom" of the sprite
-        public bool ignoreDepthSorting;
-        public string graphicsDir;
-
-
-        public bool reachedEnd { get; private set; }
         SpriteEffects effects = SpriteEffects.None;
         bool looping = true;
-        public enum OriginType { TopLeft, FromCentre, BottomLeft, BottomMiddle };
         OriginType originMode;
-        public OriginType OriginMode { get { return originMode; }}
-        public bool scaled; //if this is set to true, then the sprite is scaled up to the specified width and height
-        public int scaleWidth, scaleHeight;
-
         bool flickering; //if this is set to true, the sprite will have a random alpha set to somewhere between the upper and lower bounds every update;
         int flickerLowerBound;
         int flickerUpperBound;
         Random randFlicker;
-
+      
         public Color[] GetFrameData() //gets the current frame data as as an array
         {
-            Color[] colors = new Color[spriteSize.X * spriteSize.Y];
-            spriteSheet.GetData<Color>(0,spriteRect,colors, 0, colors.Length);
+            Color[] colors = new Color[SpriteSize.X * SpriteSize.Y];
+            SpriteSheet.GetData<Color>(0, SpriteRect, colors, 0, colors.Length);
             return colors;
         }
 
-		public Sprite(TextureLoader TextureLoader,string filename, Point pos, float d, Point spritesize, OriginType som)
+		public Sprite(TextureLoader TextureLoader, string filename, Point pos, float d, Point spritesize, OriginType som)
         {
-            drawnPosition = pos;
-            currentFrame = 0;
-            spriteRect = new Rectangle(currentFrame * spritesize.X, 0, spritesize.X, spritesize.Y);
-            spriteSize = spritesize;
+            DrawnPosition = pos;
+            CurrentFrame = 0;
+            SpriteRect = new Rectangle(CurrentFrame * SpriteSize.X, 0, SpriteSize.X, SpriteSize.Y);
             rotation = MathHelper.Pi * 0.0f;
-            depth = d;
-            interval = 190;
+            Depth = d;
+            Interval = 190;
             if (filename != null)
                 Load(filename,TextureLoader);
             originMode = som;
-            reachedEnd = false;
-            graphicsDir = filename;
-            if (graphicsDir != null)
+            ReachedEnd = false;
+            GraphicsDir = filename;
+            if (GraphicsDir != null)
             {
-                for (int i = graphicsDir.Length - 2; i > 0; i--)
+                for (int i = GraphicsDir.Length - 2; i > 0; i--)
                 {
-                    var s = graphicsDir.Substring(i, 2);
+                    var s = GraphicsDir.Substring(i, 2);
                     if (s.Contains("\\"))
                     {
-                        graphicsDir = graphicsDir.Substring(0, i);
+                        GraphicsDir = GraphicsDir.Substring(0, i);
                         break;
                     }
                 }
             }
-            if(spriteSize == Point.Zero)
-                spriteSize = new Point(spriteSheet.Width, spriteSheet.Height);
-            drawBounds = new Rectangle(drawnPosition.X, drawnPosition.Y, spriteSize.X, spriteSize.Y);
+            if(SpriteSize == Point.Zero)
+                SpriteSize = new Point(SpriteSheet.Width, SpriteSheet.Height);
+            DrawnBounds = new Rectangle(DrawnPosition.X, DrawnPosition.Y, SpriteSize.X, SpriteSize.Y);
         }
         public void SetScale(int w, int h)
         {
-            scaled = true;
+            Scaled = true;
             scaleWidth = w;
             scaleHeight = h;
-            currentFrame = 0;
+            CurrentFrame = 0;
         }
         public void SetSpriteSize() 
         {
-            if (spriteSheet != null)
+            if (SpriteSheet != null)
             {
-                spriteSize = new Point(spriteSheet.Width, spriteSheet.Height);
+                SpriteSize = new Point(SpriteSheet.Width, SpriteSheet.Height);
             }
         }
         public void SetSpriteSize(int x, int y)
         {
-            spriteSize.X = x;
-            spriteSize.Y = y;
+			SpriteSize = new Point(x, y);
         }
 
 		internal void Load(string filename, TextureLoader tl)
         {
-            spriteSheet = tl.RequestTexture(filename);
+            SpriteSheet = tl.RequestTexture(filename);
         }
         public void SetFlicker(int l, int u)
         {
             flickering = true;
             flickerLowerBound = MathHelper.Clamp(l,0,255);
             flickerUpperBound = MathHelper.Clamp(u, 0, 255);
-            randFlicker = new Random(drawnPosition.X);
+            randFlicker = new Random(DrawnPosition.X);
         }
         public void Update(GameTime gameTime)
         {
             if(flickering)
                 alpha = randFlicker.Next(flickerLowerBound,flickerUpperBound);
             timer += (float)gameTime.ElapsedGameTime.TotalMilliseconds;
-            if (timer > interval)
+            if (timer > Interval)
             {
-                if (this.spriteSheet.Width == spriteSize.X)
+                if (SpriteSheet.Width == SpriteSize.X)
                 {
                     timer = 0;
-                    reachedEnd = true;
+                    ReachedEnd = true;
                     goto UpdateSprite;
                 }
-                    currentFrame++;
-                    if (currentFrame * spriteSize.X == spriteSheet.Width - spriteSize.X)
+                    CurrentFrame++;
+                    if (CurrentFrame * SpriteSize.X == SpriteSheet.Width - SpriteSize.X)
                     {
-                        reachedEnd = true;
+                        ReachedEnd = true;
                     }
-                if (currentFrame * spriteSize.X > spriteSheet.Width - spriteSize.X)
-                {
-                    currentFrame = 0;
-                }
+                if (CurrentFrame * SpriteSize.X > SpriteSheet.Width - SpriteSize.X)
+                    CurrentFrame = 0;
                 timer = 0;
             }
             UpdateSprite:
-            if (!scaled)
-                bottomY = (drawnPosition.Y + spriteSize.Y) + bottomYOffset;
+            if (!Scaled)
+                BottomY = (DrawnPosition.Y + SpriteSize.Y) + bottomYOffset;
             else
-                bottomY = (drawnPosition.Y + scaleHeight) + bottomYOffset;
+                BottomY = (DrawnPosition.Y + scaleHeight) + bottomYOffset;
             if(originMode == OriginType.BottomMiddle)
             {
-                bottomY -= spriteSize.Y;
+                BottomY -= SpriteSize.Y;
             }
         }
         public void Draw(SpriteBatch batch)
         {
-            spriteRect = new Rectangle(currentFrame * spriteSize.X, 0, spriteSize.X, spriteSize.Y);            
-            if (scaled)
-                drawBounds = new Rectangle(drawnPosition.X, drawnPosition.Y, scaleWidth, scaleHeight);
+            SpriteRect = new Rectangle(CurrentFrame * SpriteSize.X, 0, SpriteSize.X, SpriteSize.Y);            
+            if (Scaled)
+                DrawnBounds = new Rectangle(DrawnPosition.X, DrawnPosition.Y, scaleWidth, scaleHeight);
             else
-                drawBounds = new Rectangle(drawnPosition.X, drawnPosition.Y, spriteSize.X, spriteSize.Y);
+                DrawnBounds = new Rectangle(DrawnPosition.X, DrawnPosition.Y, SpriteSize.X, SpriteSize.Y);
             switch (originMode)
             {
                 case (OriginType.TopLeft):
                     {
-                        batch.Draw(spriteSheet, drawBounds, spriteRect, new Color(255,255,255,alpha), rotation, new Vector2(0, 0), effects, depth);
+                        batch.Draw(SpriteSheet, DrawnBounds, SpriteRect, new Color(255,255,255,alpha), rotation, new Vector2(0, 0), effects, Depth);
                         break;
                     }
                 case (OriginType.FromCentre):
                     {
-                        batch.Draw(spriteSheet, drawBounds, spriteRect, new Color(255, 255, 255, alpha), rotation, new Vector2(spriteSize.X / 2, spriteSize.Y / 2), effects, depth);
+                        batch.Draw(SpriteSheet, DrawnBounds, SpriteRect, new Color(255, 255, 255, alpha), rotation, new Vector2(SpriteSize.X / 2, SpriteSize.Y / 2), effects, Depth);
                         break;
                     }
                 case (OriginType.BottomMiddle):
                     {
-                        batch.Draw(spriteSheet, drawBounds, spriteRect, new Color(255, 255, 255, alpha), rotation, new Vector2(spriteSize.X / 2, spriteSize.Y), effects, depth);
+                        batch.Draw(SpriteSheet, DrawnBounds, SpriteRect, new Color(255, 255, 255, alpha), rotation, new Vector2(SpriteSize.X / 2, SpriteSize.Y), effects, Depth);
                         break;
                     }
             }
@@ -180,18 +170,18 @@ namespace Magicians
         }
         public void Draw(SpriteBatch batch,Color color)
         {
-            spriteRect = new Rectangle(currentFrame * spriteSize.X, 0, spriteSize.X, spriteSize.Y);
-            drawBounds = new Rectangle(drawnPosition.X, drawnPosition.Y, spriteSize.X, spriteSize.Y);
+            SpriteRect = new Rectangle(CurrentFrame * SpriteSize.X, 0, SpriteSize.X, SpriteSize.Y);
+            DrawnBounds = new Rectangle(DrawnPosition.X, DrawnPosition.Y, SpriteSize.X, SpriteSize.Y);
             switch (originMode)
             {
                 case (OriginType.TopLeft):
                     {
-                        batch.Draw(spriteSheet, drawBounds, spriteRect, Color.FromNonPremultiplied(color.R, color.G, color.B, alpha), rotation, new Vector2(0, 0), effects, depth);
+                        batch.Draw(SpriteSheet, DrawnBounds, SpriteRect, Color.FromNonPremultiplied(color.R, color.G, color.B, alpha), rotation, new Vector2(0, 0), effects, Depth);
                         break;
                     }
                 case (OriginType.FromCentre):
                     {
-                        batch.Draw(spriteSheet, drawBounds, spriteRect, Color.FromNonPremultiplied(color.R, color.G, color.B, alpha), rotation, new Vector2(spriteSize.X / 2, spriteSize.Y / 2), effects, depth);
+                        batch.Draw(SpriteSheet, DrawnBounds, SpriteRect, Color.FromNonPremultiplied(color.R, color.G, color.B, alpha), rotation, new Vector2(SpriteSize.X / 2, SpriteSize.Y / 2), effects, Depth);
                         break;
                     }
             }
@@ -203,50 +193,46 @@ namespace Magicians
         }
 		public void ChangeDrawnPosition(Point point)
         {
-            drawnPosition = point;
+            DrawnPosition = point;
         }
         public void ChangeSprite(TextureLoader TextureLoader, string filename)
         {
             filename = filename.TrimStart('\\');
             this.Load(filename, TextureLoader);
-            currentFrame = 0;
-            reachedEnd = false;
-            if (graphicsDir == null)
+            CurrentFrame = 0;
+            ReachedEnd = false;
+            if (GraphicsDir == null)
             {
-                graphicsDir = filename;
+                GraphicsDir = filename;
             }
         }
         public void ChangeTexture2D(Texture2D tex)
         {
-            this.spriteSheet = tex;
-            if (spriteSheet.Width == spriteSize.X)
+            SpriteSheet = tex;
+            if (SpriteSheet.Width == SpriteSize.X)
             {
-                currentFrame = 0;
-                reachedEnd = false;
+                CurrentFrame = 0;
+                ReachedEnd = false;
             }
-            if(currentFrame * spriteSize.X > spriteSheet.Width)
+            if(CurrentFrame * SpriteSize.X > SpriteSheet.Width)
             {
-                currentFrame = 0;
-                reachedEnd = false;
+                CurrentFrame = 0;
+                ReachedEnd = false;
             }
         }
         public void ChangeTexture2D(Texture2D tex, bool resetFrame)
         {
-            this.spriteSheet = tex;
+            this.SpriteSheet = tex;
             if (resetFrame)
             {
                 timer = 0;
-                currentFrame = 0;
-                reachedEnd = false;
+                CurrentFrame = 0;
+                ReachedEnd = false;
             }
         }
         public void ChangeSpriteEffects(SpriteEffects se)
         {
             effects = se;
-        }
-        public void Dispose()
-        {
-            this.spriteSheet.Dispose();
         }
         public void ChangeLooping(bool b)
         {
@@ -254,26 +240,34 @@ namespace Magicians
         }
         public void ChangeDepth(float f)
         {
-            depth = f;
+            Depth = f;
         }
         public void SetInterval(float f)
         {
-            interval = f;
+            Interval = f;
         }
         public void SetInterval(float f, bool resetTimer)
         {
-            interval = f;
+            Interval = f;
             if (resetTimer)
                 timer = 0;
         }
         public void RandomizeFrame(Random random)
         {
-			if (spriteSheet.Width % spriteSize.X == 0)
-                currentFrame = random.Next(0, (spriteSheet.Width / spriteSize.X) - 1);
+			if (SpriteSheet.Width % SpriteSize.X == 0)
+                CurrentFrame = random.Next(0, (SpriteSheet.Width / SpriteSize.X) - 1);
         }
-        public void SetBottomYOffset(int i)
+        public void SetbottomYOffset(int i)
         {
             bottomYOffset = i;
         }
+        public void SetAlpha(int i) 
+		{
+			alpha = MathHelper.Clamp(i, 0, 255);
+		}
+		public void SetIgnoreDepthSorting(bool ignore) 
+		{
+			IgnoreDepthSorting = ignore;
+		}
     }
 }
